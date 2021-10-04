6b51f463fa9840cbaf5e9f92bd5931c3d013b1f7
Artemiy Ermilov <avermilov@edu.hse.ru>
2021-10-04 22:43:21 +0300

    added getter for loaders

---
+++ dl_module/loader.py
@@ -0,0 +1,18 @@
+from typing import Dict
+
+from torch.utils.data import DataLoader, Dataset
+
+
+def get_train_val_loader(d: Dict, train_ds: Dataset, val_ds: Dataset) -> (DataLoader, DataLoader):
+    d = d["loaders"]
+
+    train_batch_size = d["train_batch_size"]
+    train_workers = d["train_workers"]
+
+    val_batch_size = d["validation_batch_size"]
+    val_workers = d["validation_workers"]
+
+    train_loader = DataLoader(train_ds, batch_size=train_batch_size, num_workers=train_workers)
+    val_loader = DataLoader(val_ds, batch_size=val_batch_size, num_workers=val_workers)
+
+    return train_loader, val_loader
